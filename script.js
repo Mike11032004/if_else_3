041c66ffd570ebd5f86732b796eebfafc6bdbcec
Munis <xamrakulovmunis@gmail.com>
2022-11-26 01:15:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// N1
+
+
+// let userName = 'Alex'
+// let userAccount = 7777
+// let userMoney = 10000
+
+// let nameOne = prompt('What is your name?').trim()
+// if (nameOne === userName) {
+//     let accountOne = +prompt('Please enter the password!').trim()
+//     if (accountOne === userAccount) {
+//         let moneyOne = +prompt('How much money do you want to cash out?').trim()
+//         if (userMoney >= moneyOne) {
+//             alert(`Было : ${userMoney} 
+// Стало : ${userMoney - moneyOne}`)
+//         } else {
+//             alert('Not enough money! Oh you poor thing!')
+//         }
+//     } else {
+//         alert('Wrong password!')
+//     }
+// } else {
+//     alert(`User ${userName} not found!)
+// }
+
+
+
+// N2.1
+
+
+// let nameTwo = prompt('Name').trim().toUpperCase()
+// let ageTwo = +prompt('Age').trim()
+// let moneyTwo = +prompt('Money').trim()
+// let peopleTwo = +prompt('People').trim()
+
+// if (nameTwo[0] === 'A' || nameTwo[0] === 'a' && ageTwo >= 20 && ageTwo <= 40 && moneyTwo >= 100 && peopleTwo <= 10){
+//     alert(`Welcome ${nameTwo}`) 
+// } else {
+//     alert(`Go home ${nameTwo}`)
+// }
+
+
+
+// N2.2
+
+
+// let nameThree = prompt('Name').trim()
+// if (nameThree[0] === 'A'|| nameThree[0] === 'a') {
+//     let ageThree = +prompt('Age').trim()
+//     if (ageThree >= 20 && ageThree <= 40) {
+//         let moneyThree = +prompt('Money').trim()
+//         if (moneyThree >= 100) {
+//             let peopleThree = +prompt('People').trim()
+//             if (peopleThree <= 10) {
+//                 alert(`Welcome ${nameThree}`)
+//             } else {
+//                 alert(`Go home ${nameThree}`)
+//             }
+//         } else {
+//             alert(`Go home ${nameThree}`)
+//         }
+//     } else {
+//         alert(`Go home ${nameThree}`)
+//     }
+// } else {
+//     alert(`Go home ${nameThree}`)
+// }
